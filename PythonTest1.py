109031b988eda34c65e9ef200a32750021a5f823
BeardedBrain <48997377+BeardedBrain@users.noreply.github.com>
2020-05-21 16:03:41 +0300

    Add files via upload

---
+++ PythonTest1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+import pandas as pd
+
+movie_matrix = [[random.randrange(0,1) for y in range(10000)] for x in range(10000)]
+
+df_movie = pd.DataFrame(movie_matrix)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
